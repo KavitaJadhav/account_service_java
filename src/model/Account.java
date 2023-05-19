11630ef305c8535e8c60b8de3ce86d7580d5d427
KavitaJadhav <jadhavkavita206@gmail.com>
2023-05-19 08:46:31 +0530

    Reading Account details from file.

--- src/model/Account.java
+++ src/model/Account.java
@@ -3,6 +3,21 @@ package model;
 import java.math.BigDecimal;
 
 public abstract class Account {
+    public enum Type {
+        CREDIT("Credit"),
+        SAVING("Saving");
+
+        private String name;
+
+        Type(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
     protected long id;
     protected BigDecimal balance;
 
